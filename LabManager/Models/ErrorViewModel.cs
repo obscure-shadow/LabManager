6b240f3f704f33e4c90406d2e91dcfb310f7d6d5
hanna <hannahmneal@gmail.com>
2019-04-29 10:57:17 -0500

    Add project files.

---
+++ LabManager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LabManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
